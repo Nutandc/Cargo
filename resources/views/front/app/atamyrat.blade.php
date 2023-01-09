e1b4df0a5faf5296ea9cba1ac5519ae82a669eee
Atamyrat2005 <komputeratamyrat@gmail.com>
2023-01-09 16:51:58 +0500

    2.1.0

---
+++ resources/views/front/app/atamyrat.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ᗩTᗩᗰYᖇᗩT</title>
+    <link rel="icon" href="{{ asset('img/log.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <script src="{{ asset('js/Jquery.js') }}" charset="utf-8"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/aos.js') }}"></script>
+</head>
+
+<body style="background:#111; color:#fff;">
+    <h1 class="fw-bold text-center py-3" data-aos="zoom-in-down" data-aos-duration="2000">Atamyrat Shukurov</h1>
+    <div class=" fs-3 d-flex justify-content-center">
+        <a href="https://github.com/Atamyrat2005" data-aos="zoom-in-up" data-aos-duration="600"><i class="bi bi-github text-white px-3"></i></a>
+        <a href="https://t.me/programmist_hacker" data-aos="zoom-in-up" data-aos-duration="1200"><i class="bi bi-telegram text-white px-3"></i></a>
+        <a href="https://www.instagram.com/sukurow_atamyrat" data-aos="zoom-in-up" data-aos-duration="1800"><i class="bi bi-instagram text-white px-3"></i></a>
+        <a href="mailto:komputeratamyrat@gmail.com" data-aos="zoom-in-up" data-aos-duration="2400"><i class="bi bi-envelope text-white px-3"></i></a>
+        <a class="px-3 mb-1" href="https://icq.im/turkmen..hacker" data-aos="zoom-in-up" data-aos-duration="3000"><img width="27" height="27" class="img-fluid" src="{{ asset('img/icons/icq.png')}}" alt=""></a>
+    </div>
+</body>
+
+<script>
+    AOS.init();
+</script>
+
+</html>
---
+++ resources/views/front/error/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') - @lang('app.app-name')</title>
+    <link rel="icon" href="{{ asset('img/favicon2.ico') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+</head>
+@include('front.app.alert')
+<body class="bg-light">
+    @include('front.app.navbar')
+    @yield('error')
+@include('front.app.footer')
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
+<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+<!-- Template Javascript -->
+<script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
